e9ec25932615af365b1cbce17e4e93e7d9a808f5
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-10 11:26:43 +0800

    added technologies model, updated portfolio model, established associations between the two

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,4 +46,12 @@ end
 end
 
 
-puts "9 skills created"
+puts "9 portfolios created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
