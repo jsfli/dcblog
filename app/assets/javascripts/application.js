98ccc8d125d1d3215be4610106939d7250ac37db
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-17 23:41:07 +0800

    installed jquery ui, sortablehtml5 library and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require rails-ujs
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery-ui
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
