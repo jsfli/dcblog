a71a84cd0ad8f551364004b52459884b6db5d898
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-21 12:02:49 +0800

    implemented initial file uploading system via carreirwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,8 +5,11 @@ class Portfolio < ApplicationRecord
                                 reject_if: lambda {|attrs| attrs['name'].blank?}
 
   include Placeholder
-
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # carrierwave gem method for image uploading
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
 
 #########################################################################
   # CUSTOM SCOPES EXAMPLES
