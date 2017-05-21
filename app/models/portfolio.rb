46a7c38d027833980e4f99dc7fe9e5a7715329fe
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-21 14:05:16 +0800

    updated forms

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,9 +4,7 @@ class Portfolio < ApplicationRecord
                                 #data validation, reject if blank
                                 reject_if: lambda {|attrs| attrs['name'].blank?}
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
-
+  validates_presence_of :title, :body
   # carrierwave gem method for image uploading
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -23,16 +21,4 @@ class Portfolio < ApplicationRecord
   def self.by_position
     order("position ASC")
   end
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    # ||= is a short cut for the below codes
-    # if self.main_image == nil
-    #   self.main_image = "http://placehold.it/600x400"
-
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
-
 end
