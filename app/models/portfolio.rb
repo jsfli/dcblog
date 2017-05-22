8a54399869666948ce9c78fda5adb401499b59ad
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-22 12:01:59 +0800

    edited portfolio views and edit forms

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
   accepts_nested_attributes_for :technologies,
+                                allow_destroy: true,
                                 #data validation, reject if blank
                                 reject_if: lambda {|attrs| attrs['name'].blank?}
 
