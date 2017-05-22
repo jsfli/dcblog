ab31d368289a28d014906b9e2b7ff058d7051ebd
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-22 16:05:09 +0800

    Implemented twitter on job news page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'job-news', to: 'pages#job_news'
 
   resources :blogs do
     member do
