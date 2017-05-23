ebb56a043651d2e25b51afd935f02a208c1dc00e
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-23 11:25:42 +0800

    built out initial form and partial call for comments along with controller actions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,6 +17,8 @@ class BlogsController < ApplicationController
   # GET /blogs/1.json
   def show
     # default page content concerns over ride
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
