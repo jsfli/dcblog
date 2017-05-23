9a40001d9e1acda2ac6afdd32b40947dfc30796e
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-23 16:39:06 +0800

    implemented sort function in Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,11 +5,15 @@ class Blog < ApplicationRecord
 
   validates_presence_of :title, :body
 
-  belongs_to :topic
+  # belongs_to :topic
 
   has_many :comments, dependent: :destroy
 
   def self.special_blogs
     all
+  end
+
+  def self.recent
+    order("created_at DESC")
   end
 end
